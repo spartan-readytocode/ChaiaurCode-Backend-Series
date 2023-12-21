25b98d645e0c082ac462e9c9b91e5b0e5429a0fe
spartan-readytocode <7007966033abc@gmail.com>
2023-12-21 08:30:33 +0530

    added extra settings

--- src/index.js
+++ src/index.js
@@ -5,24 +5,12 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
-
-/*
-import express from "express";
-const app = express();
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("ERR", error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
     });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.log("Errow : ", error);
-    throw err;
-  }
-})();
-*/
+  })
+  .catch((err) => {
+    console.log("MongoDb connection failed");
+  });
